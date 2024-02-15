90c5af0e2389a4d2c3cfee3e147e51f3e955b743
astrasourav <souravkumarr77@gmail.com>
2024-02-15 20:37:46 +0530

    15 Feb 2024

---
+++ 12-Integer-to-Roman/12-Integer-to-Roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        if (num == 0) {
+            return "";
+        }
+
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string result = "";
+        for (int i = 0; i < values.size(); ++i) {
+            while (num >= values[i]) {
+                result += numerals[i];
+                num -= values[i];
+            }
+        }
+
+        return result;
+    }
+};
+
