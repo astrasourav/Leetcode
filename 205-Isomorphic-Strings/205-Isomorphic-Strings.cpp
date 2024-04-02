649a787d4975d9c2ad153e55ecaf55657370c9d2
astrasourav <souravkumarr77@gmail.com>
2024-04-02 12:39:56 +0530

    Updated code of question number 205

--- 205-Isomorphic-Strings/205-Isomorphic-Strings.cpp
+++ 205-Isomorphic-Strings/205-Isomorphic-Strings.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char>m1, m2;
+        if (s.length() != t.length()) {
+            return false;
+        }
 
-        for (int i=0;i <s.length(); i++) {
+        unordered_map<char, char> m1, m2;
+
+        for (int i = 0; i < s.length(); i++) {
             if (m1.find(s[i]) != m1.end()) {
                 if (m1[s[i]] != t[i]) {
                     return false;
                 }
-            } else if (m2.find(t[i]) != m2.end()) {
-                if (m2[t[i]] != s[i]) {
+            } else {
+                if (m2.find(t[i]) != m2.end()) {
                     return false;
                 }
-            } else {
                 m1[s[i]] = t[i];
                 m2[t[i]] = s[i];
             }
@@ -21,3 +24,4 @@ public:
         return true;
     }
 };
+
