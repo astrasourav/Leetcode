b8dc875941953c360ada10b69523dc1db7337527
astrasourav <souravkumarr77@gmail.com>
2024-08-08 21:59:24 +0530

    Updated Leetcode 3

--- 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.cpp
@@ -23,3 +23,28 @@ public:
         return ans;
     }
 };
+
+//Approac2 :- 
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        vector<int> mp(256, -1);
+        int l = 0, r = 0, maxLen = 0, n = s.length();
+
+        while (r < n) {
+            if (mp[s[r]] != -1) {
+                l = max(mp[s[r]] + 1, l);
+            }
+            mp[s[r]] = r;
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+
+        return maxLen;
+    }
+};
