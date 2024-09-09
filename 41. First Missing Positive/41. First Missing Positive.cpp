3e0320fca843a7ce78d837669e0ea04c0d697ec2
astrasourav <souravkumarr77@gmail.com>
2024-09-09 18:03:50 +0530

    9 sep 2024

--- 41. First Missing Positive/41. First Missing Positive.cpp
+++ 41. First Missing Positive/41. First Missing Positive.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        if (nums.size() == 0) {
-            return 0;
-        }
-
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            int ele = nums[i];
-            
+        bool contain1 = false;
 
-            if (ele >= 1 && ele <= n) {
-                int chair = ele - 1;
-                if (nums[chair] != ele) {
-                    swap(nums[chair], nums[i]);
-                    i--;
-                }
+        for (int i=0; i<n; i++) {
+            if (nums[i] == 1) {
+                contain1 = true;
+            }
+
+            if ( nums[i] <=0 || nums[i] > n) {
+                nums[i] = 1;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i + 1 != nums[i]) {
-                return i + 1;
+        if (contain1 == false) {
+            return 1;
+        }
+
+        for (int i=0; i<n; i++) {
+            int num = abs(nums[i]);
+            int idx = num -1;
+
+            if (nums[idx] < 0) continue;
+            nums[idx] *= -1;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (nums[i] > 0) {
+                return i+1;
             }
         }
 
-        return n + 1;
+        return n+1;
     }
 };
