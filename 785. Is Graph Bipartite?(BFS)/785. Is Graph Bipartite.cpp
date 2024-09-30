a4e93068516cb6d6c8ec7f1b814f7e295752777c
astrasourav <souravkumarr77@gmail.com>
2024-09-30 14:34:23 +0530

    30 Sep 2024

--- 785. Is Graph Bipartite?(BFS)/785. Is Graph Bipartite.cpp
+++ 785. Is Graph Bipartite?(BFS)/785. Is Graph Bipartite.cpp
@@ -38,3 +38,41 @@ public:
         
     }
 };
+
+---------------------------------------------
+//DFS Approach
+
+class Solution {
+public:
+    bool dfsCheck(int start, vector<int>& vis, vector<vector<int>>& graph) {
+
+        for (auto it : graph[start]) {
+            if (vis[it] == -1) {
+                vis[it] = !vis[start];
+                if (!dfsCheck(it, vis, graph)) {
+                    return false;
+                }
+            } else if (vis[it] == vis[start]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> vis(V, -1);
+
+        for (int i=0; i<V; i++)  {
+            if (vis[i] == -1) {
+                vis[i] = 0;
+                if (!dfsCheck(i, vis, graph)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
